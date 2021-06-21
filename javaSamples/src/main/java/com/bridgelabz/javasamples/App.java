909aa44eba3aba66ab93adb8f7cd912085930795
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-21 22:10:47 +0530

    adding the welcome message to file

---
+++ javaSamples/src/main/java/com/bridgelabz/javasamples/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.javasamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Line comparison computation program" );
+    }
+}
