4ca488089946d9a0c6158c45b2eee28441328551
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-21 23:21:03 +0530

    checks between 2 lines if they are equal

--- javaSamples/src/main/java/com/bridgelabz/javasamples/App.java
+++ javaSamples/src/main/java/com/bridgelabz/javasamples/App.java
@@ -10,8 +10,23 @@ public class App
     {
         System.out.println( "Welcome to Line comparison computation program" );
     
-        int x1=2,x2=3,y1=1,y2=4;
-        double LengthOfLine = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        System.out.println("Length of the line is "+LengthOfLine);
+        int Line1x1=6,Line1x2=-3,Line1y1=-1,Line1y2=4;
+        double LengthOfLine1 = Math.sqrt((Line1x2-Line1x1)*(Line1x2-Line1x1) + (Line1y2-Line1y1)*(Line1y2-Line1y1));
+        System.out.println("Length of line 1 :"+LengthOfLine1);
+        int Line2x1=-5,Line2x2=-3,Line2y1=-2,Line2y2=3;
+        double LengthOfLine2 = Math.sqrt((Line2x2-Line2x1)*(Line2x2-Line2x1) + (Line2y2-Line2y1)*(Line2y2-Line2y1));
+        System.out.println("Length of line 2 :"+LengthOfLine2);
+        
+        Double d1 = new Double(LengthOfLine1);
+        Double d2 = new Double(LengthOfLine2);
+        
+        if(d1.equals(d2))
+        {
+        	System.out.println("Both lines are equal");
+        }
+        else
+        {
+        	System.out.println("Both lines are different");
+        }
     }
 }
