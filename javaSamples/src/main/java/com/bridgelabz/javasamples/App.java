5add6b2543e6db4c614b5c05e3ae08600575782d
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-21 22:31:15 +0530

    calculated the length of the line

--- javaSamples/src/main/java/com/bridgelabz/javasamples/App.java
+++ javaSamples/src/main/java/com/bridgelabz/javasamples/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Line comparison computation program" );
+    
+        int x1=2,x2=3,y1=1,y2=4;
+        double LengthOfLine = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("Length of the line is "+LengthOfLine);
     }
 }
